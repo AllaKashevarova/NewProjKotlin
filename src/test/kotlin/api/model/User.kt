9aba2ab062c75d5c43aae63326c93b826e7391b1
Ala Kashavarava <ala.kashavarava@ideas.com>
2025-10-31 11:07:55 +0100

    Additional models

---
+++ src/test/kotlin/api/model/User.kt
@@ -0,0 +1,15 @@
+package api.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id: Long? = null,
+    val username: String,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    val phone: String? = null,
+    val userStatus: Int? = null
+)
